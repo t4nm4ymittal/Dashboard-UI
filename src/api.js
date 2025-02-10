331c8abdccf6551919e25a0533ebd198db2c8278
t4nm4ymittal <90447899+t4nm4ymittal@users.noreply.github.com>
2025-02-11 03:03:02 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000/metrics"; // Replace with your backend API
+const API_URL = "http://dashboard-backend-git-t4nm4y-dev.apps.rm3.7wse.p1.openshiftapps.com/metrics"; // Replace with your backend API
 
 export async function fetchMetrics() {
   try {
